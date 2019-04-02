1dade981a66314ac81f8bf6657e1ca46b3f0dfb0
senthilanand92 <senthilanand92@gmail.com>
2019-04-02 08:00:34 +0530

    Conditional and selfwork

---
+++ JavaBasics/src/main/java/com/tcs/conditionalStatements/ConditionalStatements.java
@@ -0,0 +1,9 @@
+package com.tcs.conditionalStatements;
+
+public class ConditionalStatements {
+
+	public ConditionalStatements() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ JavaBasics/src/main/java/com/tcs/selfwork/InitializeAndPrintAllDataTypes.java
@@ -0,0 +1,11 @@
+package com.tcs.selfwork;
+
+public class InitializeAndPrintAllDataTypes {
+
+	public InitializeAndPrintAllDataTypes() {
+		// TODO All datatypes in java - initialize and print
+		// dataype_name var_name = value; // (possbile values)  eg: int a = 10 //(-2,147,483,648 to 2,147,483, 647) ;
+		// boolean b =true; (true/false)
+	}
+
+}
